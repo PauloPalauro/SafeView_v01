42ad0eda7f2e976a8bebf37906278d2c207b3d87
pivisoto <pivisoto10@gmail.com>
2024-09-10 21:54:38 -0300

    remoção das credenciais + caminho dinamico

--- api/firebase_config.py
+++ api/firebase_config.py
@@ -1,7 +1,8 @@
 import firebase_admin
+import os
 from firebase_admin import db,credentials,firestore
-
-cred = credentials.Certificate(".\\credenciais.json")
+cred_path = os.path.join(os.path.dirname(__file__), '..', '..', 'credenciais.json')
+cred = credentials.Certificate(cred_path)
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
