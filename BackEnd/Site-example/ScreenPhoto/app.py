163a3ea765eea62c5570d738c73920bcf3f67702
PauloEduardoPalauro <paulopalauro@gmail.com>
2024-10-03 19:59:50 -0300

    Detecção de Pessoa na Webcam.

--- BackEnd/Site-example/ScreenPhoto/app.py
+++ BackEnd/Site-example/ScreenPhoto/app.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, File, UploadFile
-from fastapi.responses import FileResponse, Response, JSONResponse
+from fastapi.responses import FileResponse, Response, JSONResponse, StreamingResponse
 from pydantic import BaseModel
 import cv2
 import numpy as np
@@ -12,6 +12,8 @@ app = FastAPI()
 
 # Carregar o modelo YOLO
 model = YOLO("../../YOLO-Weights/ppe.pt")
+model_person = YOLO("yolov8n.pt")
+model_person.classes = [0]
 classNames = ['Capacete', 'Máscara', 'SEM-Capacete', 'SEM-Máscara', 'SEM-Colete', 'Pessoa', 'Colete']
 
 def analyze_image(img):
@@ -59,6 +61,59 @@ def analyze_image(img):
 
     return img, all_ok
 
+
+
+
+def generate_frames():
+    cap = cv2.VideoCapture(0)  # Inicializar captura de vídeo com a webcam padrão
+    cap.set(3, 1280)  # Definir largura
+    cap.set(4, 720)   # Definir altura
+    
+    while True:
+        # Capturar frame
+        success, img = cap.read()
+        if not success:
+            break
+        
+        # Executar o modelo YOLO no frame capturado
+        results = model_person(img, stream=True, verbose=False, classes=[0])
+
+
+        for r in results:
+            boxes = r.boxes
+            for box in boxes:
+                # Caixa delimitadora
+                x1, y1, x2, y2 = box.xyxy[0]
+                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+
+                # Confiança
+                conf = math.ceil((box.conf[0] * 100)) / 100
+                # Nome da Classe
+                cls = int(box.cls[0])
+                
+                if cls == 0 and conf > 0.5:  # Verifica se é uma pessoa e se a confiança é alta
+                    detections_found = True  # Atualiza a flag se uma detecção válida for encontrada
+                    print(f"Pessoa Detectada! Índice da classe: {cls}, Confiança: {conf}")  # Apenas imprime quando uma detecção é encontrada
+                    
+                    # Definir cor da caixa delimitadora
+                    myColor = (0, 255, 0)  # Verde
+                    cv2.rectangle(img, (x1, y1), (x2, y2), myColor, 3)
+
+                    # Desenhar texto com a confiança
+                    cvzone.putTextRect(img, f'Pessoa {conf:.2f}',  # Exibir confiança
+                                       (max(0, x1), max(35, y1)), scale=2, thickness=2,
+                                       colorB=myColor, colorT=(255, 255, 255), offset=6)
+
+            # Converter o frame para o formato JPEG
+            ret, buffer = cv2.imencode('.jpg', img)
+            frame = buffer.tobytes()
+
+            # Usar gerador para saída de frames
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+    cap.release()
+
 class ImageData(BaseModel):
     image: str
 
@@ -67,8 +122,16 @@ async def index():
     # Certifique-se de que o caminho para o index.html esteja correto
     return FileResponse("index.html")
 
+
+@app.get("/video_feed")
+async def video_feed():
+    # Gera o feed de vídeo usando StreamingResponse
+    return StreamingResponse(generate_frames(), media_type="multipart/x-mixed-replace; boundary=frame")
+
+
 @app.post("/analyze_image")
 async def analyze_image_endpoint(image_data: ImageData):
+
     # Decodificar a imagem base64 recebida
     image_bytes = base64.b64decode(image_data.image.split(",")[1])
     np_arr = np.frombuffer(image_bytes, np.uint8)
@@ -85,6 +148,7 @@ async def analyze_image_endpoint(image_data: ImageData):
     # Retornar a imagem como uma resposta HTTP junto com o status
     return JSONResponse(content={"image": base64.b64encode(buffer.tobytes()).decode('utf-8'), "all_ok": all_ok})
 
+
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8001)
