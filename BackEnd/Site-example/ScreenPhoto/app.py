8e41b611e12e544be708a733d486fdb3b18627bd
PauloEduardoPalauro <paulopalauro@gmail.com>
2024-10-05 23:15:49 -0300

    Organização do codigo e feedback da analise

--- BackEnd/Site-example/ScreenPhoto/app.py
+++ BackEnd/Site-example/ScreenPhoto/app.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI, File, UploadFile
+import asyncio
+from typing import List
+from fastapi import FastAPI, File, UploadFile, WebSocket, WebSocketDisconnect
 from fastapi.responses import FileResponse, Response, JSONResponse, StreamingResponse
 from pydantic import BaseModel
 import cv2
@@ -12,160 +14,92 @@ import os
 
 app = FastAPI()
 
-# Carregar o modelo YOLO
+# Carregar os modelos YOLO
 model = YOLO("../../YOLO-Weights/ppe.pt")
 model_person = YOLO("yolov8n.pt")
 model_person.classes = [0]
+
 classNames = ['Capacete', 'Máscara', 'SEM-Capacete', 'SEM-Máscara', 'SEM-Colete', 'Pessoa', 'Colete']
 
-def analyze_image(img):
-    # Inicializa uma flag para verificar se tudo está em ordem
+def draw_box(img, box, class_name, conf, color):
+    x1, y1, x2, y2 = map(int, box.xyxy[0])
+    cv2.rectangle(img, (x1, y1), (x2, y2), color, 3)
+    cvzone.putTextRect(img, f'{class_name} {conf:.2f}', (max(0, x1), max(35, y1)), scale=2, thickness=2, colorB=color, colorT=(255, 255, 255), offset=6)
+
+def analyze_image(img, save_path=None):
     all_ok = True
-
-    # Executar o modelo YOLO na imagem capturada
     results = model(img, stream=True)
+    
     for r in results:
-        boxes = r.boxes
-        for box in boxes:
-            # Caixa delimitadora
-            x1, y1, x2, y2 = box.xyxy[0]
-            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-            w, h = x2 - x1, y2 - y1
-
-            # Confiança
-            conf = math.ceil((box.conf[0] * 100)) / 100
-            # Nome da Classe
+        for box in r.boxes:
             cls = int(box.cls[0])
-            print(f"Índice da classe: {cls}")
+            conf = box.conf[0]
+            if cls < len(classNames) and conf > 0.5:
+                class_name = classNames[cls]
+                color = (0, 0, 255) if "SEM" in class_name else (0, 255, 0)
+                draw_box(img, box, class_name, conf, color)
+                if "SEM" in class_name:
+                    all_ok = False
 
-            if cls < len(classNames):
-                currentClass = classNames[cls]
-            else:
-                currentClass = "Desconhecido"
-            
-            print(currentClass)
-            
-            # Definir cores da caixa delimitadora e do texto
-            if conf > 0.5:
-                if currentClass in ['SEM-Capacete', 'SEM-Colete', 'SEM-Máscara']:
-                    myColor = (0, 0, 255)  # Vermelho
-                    all_ok = False  # Se algum item obrigatório estiver faltando, define all_ok como False
-                elif currentClass in ['Capacete', 'Colete', 'Máscara']:
-                    myColor = (0, 255, 0)  # Verde
-                else:
-                    myColor = (255, 0, 0)  # Azul
-
-                # Desenhar caixa delimitadora e etiqueta
-                cvzone.putTextRect(img, f'{classNames[cls]} {conf}',
-                                   (max(0, x1), max(35, y1)), scale=2, thickness=2,
-                                   colorB=myColor, colorT=(255, 255, 255), colorR=myColor, offset=6)
-                cv2.rectangle(img, (x1, y1), (x2, y2), myColor, 3)
-
+    # Salvar a imagem se o caminho for fornecido
+    if save_path:
+        cv2.imwrite(save_path, img)
+        print(f"Imagem salva em: {save_path}")
+    
     return img, all_ok
 
+async def generate_frames():
+    cap = cv2.VideoCapture(0)
+    cap.set(3, 1280)
+    cap.set(4, 720)
 
-def generate_frames():
-    cap = cv2.VideoCapture(0)  # Inicializar captura de vídeo com a webcam padrão
-    cap.set(3, 1280)  # Definir largura
-    cap.set(4, 720)   # Definir altura
-    
-    person_detected = False  # Flag para controlar a análise do YOLO
-    detection_pause_time = 0  # Tempo de pausa após a detecção
-    photo_taken = False  # Flag para controlar se a foto já foi tirada
-    analysis_paused = False  # Flag para pausar a análise após tirar a foto
+    person_detected, detection_pause_time, photo_taken, analysis_paused = False, 0, False, False
+    pause_printed = False  # Variável para controlar a exibição do print
 
     while True:
-        # Capturar frame
         success, img = cap.read()
         if not success:
             break
-        
-        current_time = time.time()  # Tempo atual
 
-        # Se a pessoa ainda não foi detectada ou a pausa já terminou, executar o modelo YOLO
+        current_time = time.time()
+
+        # Controle de pausa para detecção de pessoa
         if not person_detected or (current_time - detection_pause_time > 10):
-            # Resetar a flag após o tempo de pausa
-            if person_detected and (current_time - detection_pause_time > 10):
-                person_detected = False  # Permitir nova detecção após a pausa
-                if not photo_taken:
-                    # Tira uma foto quando os 10 segundos de pausa terminam
-                    photo_filename = f"webcam_photo_{int(current_time)}.jpg"
-                    
-                    result_img, all_ok = analyze_image(img)  # Chama a função de análise de imagem
-                    photo_filename_result = f"result_photo_{int(current_time)}.jpg"
-                    cv2.imwrite(photo_filename_result, result_img)  # Salvar a imagem analisada
-                    print(f"Foto tirada e salva como {photo_filename_result}")
-                    print("Analise volta em 10 segundos.")
-                    photo_taken = True  # Evitar tirar múltiplas fotos após a pausa
+            if person_detected and not photo_taken:
+                save_path = f"result_photo_{int(current_time)}.jpg"  # Caminho para salvar a imagem
+                result_img, all_ok = analyze_image(img, save_path=save_path)  # Salva diretamente na função
+                print("Analise volta em 10")
+                photo_taken, analysis_paused, pause_printed = True, True, False  # Reset pause_printed
+                pause_start_time = current_time
 
-                    # Iniciar uma pausa de 5 segundos antes de continuar a análise
-                    analysis_paused = True
-                    pause_start_time = current_time
-
-            # Executar o modelo YOLO no frame capturado apenas se a pausa de análise tiver terminado
+            # Verifica se o tempo de pausa acabou
             if not analysis_paused or (current_time - pause_start_time > 10):
-                analysis_paused = False  # Pausa terminou, continuar análise
+                analysis_paused = False
+                
+                if not pause_printed:  # Mostra o print apenas uma vez quando a pausa acabar
+                    print("Análise voltou")
+                    pause_printed = True  # Garante que o print não será mostrado novamente até a próxima pausa
+
                 results = model_person(img, stream=True, verbose=False, classes=[0])
-
                 for r in results:
-                    boxes = r.boxes
-                    for box in boxes:
-                        # Caixa delimitadora
-                        x1, y1, x2, y2 = box.xyxy[0]
-                        x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+                    for box in r.boxes:
+                        if int(box.cls[0]) == 0 and box.conf[0] > 0.5:
+                            person_detected, detection_pause_time, photo_taken = True, current_time, False
+                            print("Pessoa Detectada. Tirando foto em 10 segundos")
+                            draw_box(img, box, "Pessoa", box.conf[0], (0, 255, 0))
 
-                        # Confiança
-                        conf = math.ceil((box.conf[0] * 100)) / 100
-                        # Nome da Classe
-                        cls = int(box.cls[0])
-                        
-                        if cls == 0 and conf > 0.5:  # Verifica se é uma pessoa e se a confiança é alta
-                            person_detected = True  # Atualiza a flag se uma pessoa for detectada
-                            detection_pause_time = current_time  # Define o tempo da detecção
-                            photo_taken = False  # Reseta a flag para tirar nova foto após a próxima detecção
-                            print(f"Pessoa Detectada! Índice da classe: {cls}, Confiança: {conf}")  # Apenas imprime quando uma detecção é encontrada
-                            print("Foto para analise em 10 segundos")
-                            
-                            # Definir cor da caixa delimitadora
-                            myColor = (0, 255, 0)  # Verde
-                            cv2.rectangle(img, (x1, y1), (x2, y2), myColor, 3)
-
-                            # Desenhar texto com a confiança
-                            cvzone.putTextRect(img, f'Pessoa {conf:.2f}',  # Exibir confiança
-                                               (max(0, x1), max(35, y1)), scale=2, thickness=2,
-                                               colorB=myColor, colorT=(255, 255, 255), offset=6)
-
-        # Converter o frame para o formato JPEG
         ret, buffer = cv2.imencode('.jpg', img)
-        frame = buffer.tobytes()
-
-        # Usar gerador para saída de frames
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + buffer.tobytes() + b'\r\n')
 
     cap.release()
 
-
 @app.get("/", response_class=FileResponse)
 async def index():
-    # Certifique-se de que o caminho para o index.html esteja correto
     return FileResponse("index.html")
-
 
 @app.get("/video_feed")
 async def video_feed():
-    # Gera o feed de vídeo usando StreamingResponse
     return StreamingResponse(generate_frames(), media_type="multipart/x-mixed-replace; boundary=frame")
-
-
-@app.get("/result_photo/{filename}")
-async def result_photo(filename: str):
-    file_path = os.path.join(os.getcwd(), filename)
-    if os.path.exists(file_path):
-        return FileResponse(file_path)
-    else:
-        return JSONResponse(status_code=404, content={"message": "File not found"})
-
 
 if __name__ == "__main__":
     import uvicorn
